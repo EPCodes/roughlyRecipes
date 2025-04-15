03784e6b12386e61dc183d04a8f8648502b12778
Ethan <58196237+EPCodes@users.noreply.github.com>
2025-04-15 00:08:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var x = document.getElementById("nav-menu");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  } 
